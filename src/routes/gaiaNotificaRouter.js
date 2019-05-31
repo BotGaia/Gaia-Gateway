604400e63bf2227a5aeb9770fa440ca1da9b89fd
Sam Buters <sambuters@gmail.com>
2019-05-31 19:47:28 -0300

    Adding treatment to notification router
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>

--- src/routes/gaiaNotificaRouter.js
+++ src/routes/gaiaNotificaRouter.js
@@ -1,8 +1,16 @@
 const axios = require('axios');
+const notification = require('../utils/notificationTreatmentUtils');
+
+require('../config/environment');
 
 module.exports = {
   postNotification: (endpoint, json) => new Promise((resolve) => {
     const URL = `${global.URL_SPORT}/${endpoint}`;
+    const dataJson = json;
+
+    dataJson.days = notification.convertDay(dataJson.days);
+    dataJson.hoursBefore = notification.convertTimeBefore(dataJson.hoursBefore);
+    dataJson.minutesBefore = notification.convertTimeBefore(dataJson.minutesBefore);
 
     axios.post(URL, json).then((res) => {
       resolve(res.data);
